551f9096c909d2cb1f8297ca50d24600b1ca4681
MxVaibs <mvaibhav.main@gmail.com>
2025-10-14 21:03:37 +0530

    Started Product Viewer Section myself.

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -3,57 +3,51 @@ import { useMacBookStore } from '../store'
 import clsx from 'clsx'
 
 const ProductViewer = () => {
-  const { color, scale, setColor, setScale } = useMacBookStore()
+  const { color, setColor, scale, setScale } = useMacBookStore()
+
   return (
     <section id="product-viewer">
       <h2>Take a closer look</h2>
 
       <div className="controls">
         <p>
-          MacBook {scale} in {color}.
+          MacBookPro {scale}" in Space {color}
         </p>
-        <div className=" flex-center gap-5 mt-5">
+
+        <div className="flex-center gap-5 mt-5">
           <div className="color-control">
             <div
-              onClick={() => {
-                setColor('#abd5bd')
-              }}
+              onClick={() => setColor('#adb5bd')}
               className={clsx(
-                ' bg-neutral-300',
-                color === '#abd5bd' && 'active'
+                'bg-neutral-300',
+                color === '#adb5bd' && 'active'
               )}
-            ></div>
+            />
             <div
-              onClick={() => {
-                setColor('#2e2c2e')
-              }}
+              onClick={() => setColor('#2e2c2e')}
               className={clsx(
-                ' bg-neutral-900',
+                'bg-neutral-900',
                 color === '#2e2c2e' && 'active'
               )}
-            ></div>
+            />
           </div>
 
           <div className="size-control">
             <div
-              onClick={() => {
-                setScale(0.06)
-              }}
+              onClick={() => setScale(0.06)}
               className={clsx(
                 scale === 0.06
                   ? 'bg-white text-black'
-                  : ' bg-transparent text-white'
+                  : 'bg-transparent text-white'
               )}
             >
               <p>14"</p>
             </div>
             <div
-              onClick={() => {
-                setScale(0.08)
-              }}
+              onClick={() => setScale(0.08)}
               className={clsx(
                 scale === 0.08
-                  ? ' bg-white text-black'
+                  ? 'bg-white text-black'
                   : 'bg-transparent text-white'
               )}
             >
--- src/store/index.js
+++ src/store/index.js
@@ -7,5 +7,5 @@ export const useMacBookStore = create((set) => ({
   scale: 0.08,
   setScale: (scale) => set({ scale }),
 
-  reset: () => set({ color: '#2e2c3e', scale: 0.08 }),
+  reset: () => set({ color: '#2e2c2e', scale: 0.08 }),
 }))
